5632d257231ff11d8017a61da76636e50472c0b6
Palacios lucas <palacioslucasm912@gmail.com>
2023-07-19 17:34:05 -0300

    carga inicial

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Users from "@/components/Users"
+async function fetchUsers(){
+  const res= await fetch("https://reqres.in/api/users")
+  const data = await res.json()
+  return data.data
+}
+async function IndexPage() {
+  const users = await fetchUsers();
+
+  return  <Users users={users}></Users>
+   
+  
+}
+
+export default IndexPage
